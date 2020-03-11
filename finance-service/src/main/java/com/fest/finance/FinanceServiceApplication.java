eab551f941ce4af97169af6baf471ea76d278b97
yesitao <fateyst@163.com>
2020-03-11 10:47:51 +0800

    sba added

--- finance-service/src/main/java/com/fest/finance/FinanceServiceApplication.java
+++ finance-service/src/main/java/com/fest/finance/FinanceServiceApplication.java
@@ -17,7 +17,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
+//        DataSourceAutoConfiguration.class,
         JndiDataSourceAutoConfiguration.class,
         JmxAutoConfiguration.class,
         SpringApplicationAdminJmxAutoConfiguration.class,
--- finance-service/src/main/java/com/fest/finance/domain/entity/CloudAssetKey.java
+++ finance-service/src/main/java/com/fest/finance/domain/entity/CloudAssetKey.java
@@ -1,5 +1,6 @@
 package com.fest.finance.domain.entity;
 
+
 public class CloudAssetKey {
     /**
      *  
@@ -26,4 +27,5 @@ public class CloudAssetKey {
     public void setAssetId(Integer assetId) {
         this.assetId = assetId;
     }
+
 }
